1aa4eaa040c465b597e6cd2bc12436c67058cceb
Jorge Velasco <jorge@typeform.com>
2018-03-03 21:26:05 +0100

    Replace storage mock for in memory implementation

--- src/AvailabilityStrategy/FixedWaitTimeToRetryTest.php
+++ src/AvailabilityStrategy/FixedWaitTimeToRetryTest.php
@@ -15,62 +15,65 @@ final class FixedWaitTimeToRetryTest extends TestCase
     /** @test */
     public function it_report_as_available()
     {
-        $storage = $this->prophesize(Storage::class);
+        $storage = new InMemoryStorage();
         $strategy = new FixedWaitTimeToRetry(
-            $storage->reveal(),
+            $storage,
             self::MAX_FAILURES,
             self::ONE_SECOND
         );
 
-        $storage->numberOfFailures(self::SERVICE_NAME)->willReturn(self::MAX_FAILURES-1);
+        $storage->setNumberOfFailures(self::SERVICE_NAME, self::MAX_FAILURES-1);
         $this->assertTrue($strategy->isAvailable(self::SERVICE_NAME));
     }
 
     /** @test */
     public function it_reports_as_non_available()
     {
-        $storage = $this->prophesize(Storage::class);
+        $storage = new InMemoryStorage();;
         $strategy = new FixedWaitTimeToRetry(
-            $storage->reveal(),
+            $storage,
             self::MAX_FAILURES,
             self::ONE_SECOND
         );
 
-        $storage->numberOfFailures(self::SERVICE_NAME)->willReturn(self::MAX_FAILURES);
-        $storage->getStrategyData($strategy, self::SERVICE_NAME, self::LAST_ATTEMPT_KEY)->willReturn("");
+        $storage->setNumberOfFailures(self::SERVICE_NAME, self::MAX_FAILURES);
         $this->assertFalse($strategy->isAvailable(self::SERVICE_NAME));
     }
 
     /** @test */
     public function it_close_the_circuit_after_timeout()
     {
-        $storage = $this->prophesize(Storage::class);
+        $storage = new InMemoryStorage();
         $strategy = new FixedWaitTimeToRetry(
-            $storage->reveal(),
+            $storage,
             self::MAX_FAILURES,
             self::ONE_SECOND
         );
 
-        $storage->numberOfFailures(self::SERVICE_NAME)->willReturn(self::MAX_FAILURES);
+        $storage->setNumberOfFailures(self::SERVICE_NAME, self::MAX_FAILURES);
         $oneSecAndOneMillisecond = floor((microtime(true) * 1000)) - 1001;
-        $storage->getStrategyData($strategy, self::SERVICE_NAME, self::LAST_ATTEMPT_KEY)
-            ->willReturn((string) $oneSecAndOneMillisecond);
-        $storage->resetFailuresCounter(self::SERVICE_NAME)->shouldBeCalledTimes(1);
+        $storage->saveStrategyData(
+            $strategy,
+            self::SERVICE_NAME,
+            self::LAST_ATTEMPT_KEY,
+            (string) $oneSecAndOneMillisecond
+        );
 
         $this->assertTrue($strategy->isAvailable(self::SERVICE_NAME));
+        $this->assertEquals(0, $storage->numberOfFailures(self::SERVICE_NAME));
     }
 
     /** @test */
     public function it_reports_as_available_on_storage_failures()
     {
-        $storage = $this->prophesize(Storage::class);
+        $storage = new InMemoryStorage();
         $strategy = new FixedWaitTimeToRetry(
-            $storage->reveal(),
+            $storage,
             self::MAX_FAILURES,
             self::ONE_SECOND
         );
 
-        $storage->numberOfFailures(self::SERVICE_NAME)->willThrow(StorageException::class);
+        $storage->throwExceptionInNumberOfFailures();
         $this->assertTrue($strategy->isAvailable(self::SERVICE_NAME));
     }
 }
---
+++ src/AvailabilityStrategy/InMemoryStorage.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace JVelasco\CircuitBreaker\AvailabilityStrategy;
+
+use JVelasco\CircuitBreaker\AvailabilityStrategy;
+use JVelasco\CircuitBreaker\StorageException;
+
+class InMemoryStorage implements Storage
+{
+    private $numberOfFailures = [];
+    private $strategyData = [];
+    private $throwExceptionInNumberOfFailures;
+
+    public function numberOfFailures(string $serviceName): int
+    {
+        if ($this->throwExceptionInNumberOfFailures) {
+            throw new StorageException();
+        }
+
+        return $this->numberOfFailures[$serviceName];
+    }
+
+    public function resetFailuresCounter(string $serviceName)
+    {
+        $this->numberOfFailures[$serviceName] = 0;
+    }
+
+    public function saveStrategyData(
+        AvailabilityStrategy $strategy,
+        string $serviceName,
+        string $key,
+        string $value
+    ) {
+        if (!isset($this->strategyData[$strategy->getId()])) {
+            $this->strategyData[$strategy->getId()] = [$serviceName => []];
+        } elseif (!isset($this->strategyData[$strategy->getId()][$serviceName])) {
+            $this->strategyData[$strategy->getId()][$serviceName] = [];
+        }
+
+        $this->strategyData[$strategy->getId()][$serviceName][$key] =  $value;
+    }
+
+    public function getStrategyData(
+        AvailabilityStrategy $strategy,
+        string $serviceName,
+        string $key
+    ): string
+    {
+        if (empty($this->strategyData[$strategy->getId()][$serviceName])) {
+            return "";
+        }
+
+        return $this->strategyData[$strategy->getId()][$serviceName][$key];
+    }
+
+    public function setNumberOfFailures($serviceName, $numberOfFailures)
+    {
+        $this->numberOfFailures[$serviceName] = $numberOfFailures;
+        return $this;
+    }
+
+    public function throwExceptionInNumberOfFailures()
+    {
+        $this->throwExceptionInNumberOfFailures = true;
+    }
+}
